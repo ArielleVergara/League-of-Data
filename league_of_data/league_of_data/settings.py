a2e032d513dd52387c34077717abf0a7151be734
pablogonzamad <pa.gonzalezm@duocuc.cl>
2024-05-05 10:26:12 -0400

    Cambio de Graphs a Chart.js

--- league_of_data/league_of_data/settings.py
+++ league_of_data/league_of_data/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&07w7&*8lfog_*9!#^m6#&3hhl90_(%0ghg41c0y^7s=4cs2kh'
 
 #Editar API cada 24hrs
-RIOT_API_KEY = ''
+RIOT_API_KEY = 'RGAPI-268a941a-67ac-4ac4-a539-6e9a27a39c43'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
