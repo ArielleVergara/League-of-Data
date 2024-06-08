2859468113a5a0d2f157b210dc44f60f4cb62bfd
ArielleVergara <ariellefvergarad@gmail.com>
2024-06-07 23:16:04 -0400

    se incorpora el acordión para la visualización de partidas

--- league_of_data/league_of_data/settings.py
+++ league_of_data/league_of_data/settings.py
@@ -85,6 +85,12 @@ DATABASES = {
     }
 }
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'LOCATION': 'unique-snowflake',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
